0e063ffdf26cdd5d084146ec395bb986611fbe1b
Jakob Gray <jdg7sh@virginia.edu>
2020-11-05 13:35:29 -0500

    Remove copied files

--- cicd-scripts/chartAutomation.sh
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2020 Red Hat, Inc.
-
-#Script to be run in travis when charts are updated in master
-
-cicd-scripts/install-dependencies.sh
-cicd-scripts/chart-sync.sh
-git add .
-git commit -m "Update from $1"
-git push https://${MCH_REPO_BOT_TOKEN}@github.com/open-cluster-management/multicloudhub-repo.git "master"
