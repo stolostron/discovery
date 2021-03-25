c75c3b2853eca698a61cfc1faa47bc4a8f455edf
Jakob Gray <20209054+JakobGray@users.noreply.github.com>
2021-03-25 16:07:45 -0400

    Remove commented out code and delete AfterSuite, which is now handled by AfterEach

--- test/e2e/e2e.go
+++ test/e2e/e2e.go
@@ -19,7 +19,6 @@ limitations under the License.
 package e2e
 
 import (
-	"context"
 	"flag"
 	"testing"
 
@@ -28,15 +27,12 @@ import (
 	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
-	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	discoveryv1 "github.com/open-cluster-management/discovery/api/v1"
@@ -46,8 +42,6 @@ import (
 // These tests use Ginkgo (BDD-style Go testing framework). Refer to
 // http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
 
-var cfg *rest.Config
-var testEnv *envtest.Environment
 var reportFile string
 
 var (
@@ -91,44 +85,4 @@ var _ = BeforeSuite(func() {
 	Expect(err).ToNot(HaveOccurred())
 	Expect(k8sClient).ToNot(BeNil())
 
-})
-
-// var _ = BeforeSuite(func() {
-// 	// Add any setup steps that needs to be executed before each test
-// 	By("Cleaning up test objects")
-// 	ctx := context.Background()
-
-// 	discoveryRefresh := &discoveryv1.DiscoveredClusterRefresh{}
-// 	k8sClient.DeleteAllOf(ctx, discoveryRefresh, client.InNamespace("open-cluster-management"))
-
-// 	discoveryConfig := &discoveryv1.DiscoveryConfig{}
-// 	k8sClient.DeleteAllOf(ctx, discoveryConfig, client.InNamespace("open-cluster-management"))
-
-// 	discoveredCluster := &discoveryv1.DiscoveredCluster{}
-// 	k8sClient.DeleteAllOf(ctx, discoveredCluster, client.InNamespace("open-cluster-management"))
-
-// 	secretKey := types.NamespacedName{Name: SecretName, Namespace: DiscoveryNamespace}
-// 	secret := &corev1.Secret{}
-// 	_ = k8sClient.Get(ctx, secretKey, secret)
-// 	k8sClient.Delete(ctx, secret)
-// })
-
-var _ = AfterSuite(func() {
-	// Add any teardown steps that needs to be executed after each test
-	By("Cleaning up test objects")
-	ctx := context.Background()
-
-	discoveryRefresh := &discoveryv1.DiscoveredClusterRefresh{}
-	k8sClient.DeleteAllOf(ctx, discoveryRefresh, client.InNamespace("open-cluster-management"))
-
-	discoveryConfig := &discoveryv1.DiscoveryConfig{}
-	k8sClient.DeleteAllOf(ctx, discoveryConfig, client.InNamespace("open-cluster-management"))
-
-	discoveredCluster := &discoveryv1.DiscoveredCluster{}
-	k8sClient.DeleteAllOf(ctx, discoveredCluster, client.InNamespace("open-cluster-management"))
-
-	secretKey := types.NamespacedName{Name: SecretName, Namespace: discoveryNamespace}
-	secret := &corev1.Secret{}
-	_ = k8sClient.Get(ctx, secretKey, secret)
-	k8sClient.Delete(ctx, secret)
 })
