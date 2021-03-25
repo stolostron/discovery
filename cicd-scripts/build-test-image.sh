fdd844ff0e0877ba5f5d04d1c0ac8cb031663e76
Jakob Gray <20209054+JakobGray@users.noreply.github.com>
2021-03-25 14:40:22 -0400

    Remove script to build test image (make tests/docker-build can be used instead)

--- cicd-scripts/build-test-image.sh
+++
@@ -1,8 +0,0 @@
-# Copyright Contributors to the Open Cluster Management project
-
-#!/bin/bash
-# Copyright (c) 2020 Red Hat, Inc.
-
-echo "Building test-image"
-
-docker build . -f integration_tests/build/Dockerfile -t $1
