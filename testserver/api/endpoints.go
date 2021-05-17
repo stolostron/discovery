f08861fe9783477945dca15f873ada546ac52b71
Jakob Gray <20209054+JakobGray@users.noreply.github.com>
2021-05-17 10:00:18 -0400

    Add route groups for test scenarios
    This will allow all scenarios to be accessible at once. Different
    scenarios can be accessed by including their name in the base path.

--- testserver/api/endpoints.go
+++ testserver/api/endpoints.go
@@ -10,4 +10,16 @@ func SetupEndpoints(r *gin.Engine, logger zerolog.Logger) {
 	r.GET("/scenarios/:scenario", SetScenario)
 	r.GET("/api/accounts_mgmt/v1/subscriptions", GetSubscriptions)
 	r.POST("/auth/realms/redhat-external/protocol/openid-connect/token", GetToken)
+
+	// Add route group for each scenario
+	for k := range scenarios {
+		addRoutes(r.Group(k), k)
+	}
+
+}
+
+func addRoutes(rg *gin.RouterGroup, scenario string) {
+	s := Scenario(scenario)
+	rg.GET("/api/accounts_mgmt/v1/subscriptions", s.GetSubscriptions)
+	rg.POST("/auth/realms/redhat-external/protocol/openid-connect/token", GetToken)
 }
--- testserver/api/subscriptions.go
+++ testserver/api/subscriptions.go
@@ -12,6 +12,12 @@ import (
 	"github.com/open-cluster-management/discovery/pkg/ocm/subscription"
 )
 
+type Scenario string
+
+func (s Scenario) Path() string {
+	return scenarios[string(s)]
+}
+
 // GetSubscriptions ...
 func GetSubscriptions(c *gin.Context) {
 	header := c.Request.Header["Authorization"]
@@ -29,6 +35,45 @@ func GetSubscriptions(c *gin.Context) {
 		file, err = ioutil.ReadFile(path.Join(dataFolder, "connection2/subscription_response.json"))
 	} else {
 		file, err = ioutil.ReadFile(paginate(path.Join(dataFolder, "subscription_response.json"), page))
+	}
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"msg": fmt.Sprintf("Error reading file: %s", err.Error()),
+		})
+		return
+	}
+
+	// Validate file can be unmarshalled into SubscriptionResponse
+	var subscriptionList subscription.SubscriptionResponse
+	err = json.Unmarshal(file, &subscriptionList)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"msg": fmt.Sprintf("Error unmarshalling JSON: %s", err.Error()),
+		})
+		return
+	}
+
+	c.Data(http.StatusOK, "application/json", file)
+}
+
+// Behaves the same as GetSubscriptions, but using a set scenario rather than the dataFolder var
+func (s Scenario) GetSubscriptions(c *gin.Context) {
+	header := c.Request.Header["Authorization"]
+	auth := strings.Join(header, " ")
+	params := c.Request.URL.Query()
+	page := params.Get("page")
+
+	var file []byte
+	var err error
+	if strings.Contains(auth, "connection1") {
+		fmt.Println("Returning connection1 response")
+		file, err = ioutil.ReadFile(path.Join(s.Path(), "connection1/subscription_response.json"))
+	} else if strings.Contains(auth, "connection2") {
+		fmt.Println("Returning connection2 response")
+		file, err = ioutil.ReadFile(path.Join(s.Path(), "connection2/subscription_response.json"))
+	} else {
+		file, err = ioutil.ReadFile(paginate(path.Join(s.Path(), "subscription_response.json"), page))
 	}
 
 	if err != nil {
