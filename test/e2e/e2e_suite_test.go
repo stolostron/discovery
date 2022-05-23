58c6375904b7e6b7610a7102c02aafb7708c6d9a
Ray Harris <raharris@redhat.com>
2022-05-23 11:21:43 -0400

    test/e2e: allow certain tests to be skipped in canary runs
    Signed-off-by: Ray Harris <raharris@redhat.com>

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -22,29 +22,29 @@ import (
 	"flag"
 	"testing"
 
-	. "github.com/onsi/ginkgo"
-	"github.com/onsi/ginkgo/reporters"
-	. "github.com/onsi/gomega"
+	discovery "github.com/stolostron/discovery/api/v1"
+
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	ctrl "sigs.k8s.io/controller-runtime"
-
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
-	discovery "github.com/stolostron/discovery/api/v1"
+	. "github.com/onsi/ginkgo"
+	"github.com/onsi/ginkgo/reporters"
+	. "github.com/onsi/gomega"
 	// +kubebuilder:scaffold:imports
 )
 
 // These tests use Ginkgo (BDD-style Go testing framework). Refer to
 // http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
 
-var reportFile string
-
 var (
+	reportFile         string
+	inCanary           bool
 	scheme             = runtime.NewScheme()
 	DiscoveryNamespace = flag.String("namespace", "open-cluster-management", "The namespace to run tests")
 	BaseURL            = flag.String("baseURL", "", "Service to reach mock server")
@@ -57,11 +57,18 @@ func init() {
 
 	utilruntime.Must(corev1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
+
+	flag.StringVar(&reportFile, "report-file", "./results/e2e-results.xml", "Provide the path to where the junit results will be printed.")
+	flag.BoolVar(&inCanary, "inCanary", false, "The e2e tests are running in a canary environment")
+
+	flag.Parse()
 }
 
-func init() {
-	flag.StringVar(&reportFile, "report-file", "./results/e2e-results.xml", "Provide the path to where the junit results will be printed.")
-	// flag.StringVar(&discoveryNamespace, "namespace", "open-cluster-management", "The namespace to run tests")
+func TestE2E(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode.")
+	}
+	RunE2ETests(t)
 }
 
 func RunE2ETests(t *testing.T) {
